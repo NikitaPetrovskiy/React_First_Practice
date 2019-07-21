cd4d406a136127e94f1415a746ea7c40fb4e455f
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-21 16:41:17 +0300

    Began to create a project using React. Created three React components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <article className="content">
+            <section>
+                <img src="http://www.onwebbox.com/Content/onwebbox.com/Mngt/RealImage/834Online-Banner-Maker.jpg" alt="banner"/>
+            </section>
+            <section>
+                avatar + descr
+            </section>
+            <section>
+                my posts
+                <section>
+                    new post
+                </section>
+                <section>
+                    <div>post1</div>
+                    <div>post2</div>
+                </section>
+            </section>
+
+        </article>
+    )
+};
+
+export default Profile;

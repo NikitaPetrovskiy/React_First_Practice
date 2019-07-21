0b3544e5df0d83ff4b6ee310ba841ea37594bec2
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-21 15:37:48 +0300

    Created two test React components

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header () {
+    return (
+        <header>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contacts</a>
+        </header>
+    )
+};
+
+export default Header;

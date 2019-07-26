82b048dba6e5949ff97d4d847ebf80703b6983ff
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-07-26 14:35:04 +0300

    Created two new block 'dialogs' abnd 'messages' in 'Dialogs' component and added css styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,39 @@ import styles from './Dialogs.module.css';
 const Dialogs = (props) => {
     return (
         <section>
-            Dialogs
+            <div className={styles.dialogs}>
+                <div className={styles.dialogsItems}>
+                    <div className={`${styles.dialog} ${styles.active}`}>
+                        Sasha
+                    </div>
+                    <div className={styles.dialog} >
+                        Dima
+                    </div>
+                    <div className={styles.dialog}>
+                        Andrey
+                    </div>
+                    <div className={styles.dialog}>
+                        Masha
+                    </div>
+                    <div className={styles.dialog}>
+                        Jenia
+                    </div>
+                </div>
+                <div className={styles.messages}>
+                    <div className={styles.message}>
+                        Hi!
+                    </div>
+                    <div className={styles.message}>
+                        Hello
+                    </div>
+                    <div className={styles.message}>
+                        Thank you!
+                    </div>
+                    <div className={styles.message}>
+                        Good bye!
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }

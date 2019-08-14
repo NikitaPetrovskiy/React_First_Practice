49889953e7d309869c14417dc9b863043d1ade3d
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-08-14 15:21:54 +0300

    Installed 'react-redux', added 'Provider' and 'connect' from 'react-redux', refactored components and created context

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux_store';
 import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
 
 let rerenderEntireThree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
+            <Provider store={store}>
+                <App />
+            </Provider>
         </BrowserRouter>, document.getElementById('root'));
 }
 
